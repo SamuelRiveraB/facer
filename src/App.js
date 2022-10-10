bc8f94c742ab650e9d12d1b9e9938afe1f3289f7
SamuelRiveraB <114023422+SamuelRiveraB@users.noreply.github.com>
2022-10-10 10:44:29 -0500

    Add starting files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from "./Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/*<Logo />
+      <ImageLinkForm />
+  <FaceRecognition />+*/}
+    </div>
+  );
+}
+
+export default App;
