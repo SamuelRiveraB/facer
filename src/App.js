5e65b325b83d2ce4d47a38a1ff921f6cc72ef059
SamuelRiveraB <114023422+SamuelRiveraB@users.noreply.github.com>
2022-10-10 13:04:21 -0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import Navigation from "./Navigation";
+import Navigation from "./components/Navigation";
+import ImageLinkForm from "./components/ImageLinkForm";
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      {/*<Logo />
+      <ParticlesBg type="square" color={["#ff0000"]}  num={20} bg={true} />
+      <Navigation name="Sam"/>
       <ImageLinkForm />
-  <FaceRecognition />+*/}
+  {/*<FaceRecognition />*/}
     </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "./Logo";
+import "./Navigation.css";
 
 function Navigation(props) {
-    return <p>Hello, {props.name}</p>
+    return <div>
+        <nav style={{display: "flex"}}>
+            <Logo />
+            <p className="f3 link dim black underline pa3 pointer to-right">Sign out</p>
+        </nav>
+        <p>Hello, {props.name}</p>
+    </div>
 }
 
 export default Navigation;
